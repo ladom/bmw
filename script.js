417afc02a2906f4502d6b3ba4a494d58977c8447
Andrzej Ładomirski <ladomirski@gmail.com>
2017-04-03 18:07:45 +0200

    fixed

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ console.log(topImageHeight);
 	$('#carousel ul').css({width: ulLength});
 	$('#carousel li').css({width: ulWidth});
 	$('#controls').css({width: ulWidth});
-	$('#controls').css({width: ulWidth});
 	$('#controls').css({top: topImageHeight});
 	
 	$(window).resize(function(){
-		$('#carousel').css({width: ulWidth});
-		$('#carousel ul').css({width: ulLength});
-		$('#carousel li').css({width: ulWidth});
-		$('#controls').css({width: ulWidth});
-		$('#controls').css({top: topImageHeight});
+		var ulWidth1 = $(window).width() * 0.9;
+		var ulLength1 = ulWidth1 * imageList;
+		var topImageHeight1 = $('#carousel img').height() / 2;
+		$('#carousel').css({width: ulWidth1});
+		$('#carousel ul').css({width: ulLength1});
+		$('#carousel li').css({width: ulWidth1});
+		$('#controls').css({width: ulWidth1});
+		$('#controls').css({top: topImageHeight1});
 	});
 
 	console.log($(window).width());	
